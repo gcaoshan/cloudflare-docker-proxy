767d16cc129189383d8656c5abdfd3f1597ce87e
goral.cao <goral.cao@gmail.com>
2024-09-20 11:43:27 +0800

    update domain

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.ninebotcode.xyz": dockerHub,
+  "quay.ninebotcode.xyz": "https://quay.io",
+  "gcr.ninebotcode.xyz": "https://gcr.io",
+  "k8s-gcr.ninebotcode.xyz": "https://k8s.gcr.io",
+  "k8s.ninebotcode.xyz": "https://registry.k8s.io",
+  "ghcr.ninebotcode.xyz": "https://ghcr.io",
+  "cloudsmith.ninebotcode.xyz": "https://docker.cloudsmith.io",
+  "ecr.ninebotcode.xyz": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.ninebotcode.xyz": dockerHub,
 };
 
 function routeByHosts(host) {
